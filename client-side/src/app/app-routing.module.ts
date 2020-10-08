b824440b1438618917fddb3d325b698531525b0d
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-08 18:33:19 +0300

    added add-animal to routing tree

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddAnimalComponent } from './animals/add-animal/add-animal.component';
 import { AnimalsComponent } from './animals/animals.component';
 
 const routes: Routes = [
-  { path: '', component: AnimalsComponent }
+  { path: '', component: AnimalsComponent },
+  { path: 'add-animal', component: AddAnimalComponent }
 ];
 
 @NgModule({
