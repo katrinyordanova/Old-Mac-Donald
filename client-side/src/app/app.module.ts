b4e64e722acbe189ff027c2777148d9ddd418fe4
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-08 16:28:44 +0300

    added animal component to app.module

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AnimalsComponent } from './animals/animals.component';
+import { AnimalComponent } from './animals/animal/animal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AnimalsComponent
+    AnimalsComponent,
+    AnimalComponent
   ],
   imports: [
     BrowserModule,
