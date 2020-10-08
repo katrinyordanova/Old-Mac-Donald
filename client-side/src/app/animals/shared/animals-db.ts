f22c967b63c129848e943884ddd54f25bd985e50
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-08 16:24:05 +0300

    created animals db

---
+++ client-side/src/app/animals/shared/animals-db.ts
@@ -0,0 +1,26 @@
+import { Animal } from './animal';
+
+export let Animals_DB = {
+    animals: [
+        {
+            name: 'cow',
+            sound: 'moo'
+        }, 
+        {
+            name: 'chicks',
+            sound: 'cluck'
+        },
+        {
+            name: 'sheep',
+            sound: 'baa'
+        },
+        {
+            name: 'pig',
+            sound: 'oink'
+        },
+        {
+            name: 'horse',
+            sound: 'neigh'
+        }
+    ] as Animal[]
+}
