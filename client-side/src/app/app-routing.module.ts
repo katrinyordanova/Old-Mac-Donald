a91249d145ab1fcc0409edc3d4c07b9db3e6c0b9
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-08 15:10:21 +0300

    added routing path for animals component

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimalsComponent } from './animals/animals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AnimalsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
