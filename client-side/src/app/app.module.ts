d224ac01b4f9dc82d544566d45343bb525b1ba88
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-09 18:36:43 +0300

    added FormModule to app.module

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AnimalsComponent } from './animals/animals.component';
@@ -18,6 +19,7 @@ import { AddAnimalComponent } from './animals/add-animal/add-animal.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
