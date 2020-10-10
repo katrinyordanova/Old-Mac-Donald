002bce735e605af97ad5fed04f078b75b1b6ae39
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-10 21:31:04 +0300

    restructured add-animal.specs.ts and added 4 unit tests

--- client-side/src/app/animals/add-animal/add-animal.component.spec.ts
+++ client-side/src/app/animals/add-animal/add-animal.component.spec.ts
@@ -1,25 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { AnimalService } from '../shared/animal.service';
 
 import { AddAnimalComponent } from './add-animal.component';
 
-describe('AddAnimalComponent', () => {
-  let component: AddAnimalComponent;
-  let fixture: ComponentFixture<AddAnimalComponent>;
+let fixture: ComponentFixture<AddAnimalComponent>;
+let addAnimalComponent: AddAnimalComponent;
+let page: Page;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddAnimalComponent ]
-    })
-    .compileComponents();
+describe('AddAnimalComponent', () => {
+  describe('with FormsModule setup', formsModuleSetUp );
+});
+
+function formsModuleSetUp() {
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule ],
+      declarations: [ AddAnimalComponent ],
+      providers: [
+        { provide: AnimalService }
+      ]
+    }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddAnimalComponent);
-    component = fixture.componentInstance;
+  beforeEach(async() => { 
+    createComponent(); 
+  });
+
+  it('should have add button', waitForAsync(() => {
+    expect(page.addAnimalButton).toBeDefined();
+  }));
+  
+  it('should have name input filed', waitForAsync(() => {
+    expect(page.nameInput).toBeDefined();
+  }));
+
+  it('should have sound input field', waitForAsync(() => {
+    expect(page.soundInput).toBeDefined();
+  }));
+
+  it('add button should be disabled initially', waitForAsync(() => {
+    expect(page.addAnimalButton.disabled).toBe(false);
+  }));
+}
+
+function createComponent() {
+  fixture = TestBed.createComponent(AddAnimalComponent);
+  addAnimalComponent = fixture.componentInstance;
+  page = new Page(fixture);
+  fixture.detectChanges();
+  return fixture.whenStable().then(() => {
     fixture.detectChanges();
   });
+}
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+class Page {
+  get addAnimalButton() {
+    return this.query<HTMLButtonElement>('.add-animal__form__button__add-animal');
+  }
+
+  get nameInput() {
+    return this.query<HTMLElement>('.add-animal__form__animal-name__input');
+  }
+
+  get soundInput() {
+    return this.query<HTMLElement>('.add-animal__form__animal-sound__input');
+  }
+
+  constructor(fixture: ComponentFixture<AddAnimalComponent>) {}
+
+  private query<T>(selector: string): T {
+    return fixture.nativeElement.querySelector(selector);
+  }
+}
