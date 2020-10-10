28af25ef017217a8f32c8f80855200cb586f8819
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-10 20:17:36 +0300

    added ? before name and sound to check if provided into animal template

--- client-side/src/app/animals/animal/animal.component.spec.ts
+++ client-side/src/app/animals/animal/animal.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AnimalComponent } from './animal.component';
 
 describe('AnimalComponent', () => {
-  let component: AnimalComponent;
+  let animalComponent: AnimalComponent;
   let fixture: ComponentFixture<AnimalComponent>;
 
   beforeEach(async () => {
@@ -15,11 +15,11 @@ describe('AnimalComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AnimalComponent);
-    component = fixture.componentInstance;
+    animalComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should instantiate component', () => {
+    expect(animalComponent).toBeTruthy();
   });
 });
