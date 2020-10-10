3381b167dd6cfe732cbdf9675636b644e8a4ccd9
Katrin Yordanova <katrinyordanova@abv.bg>
2020-10-10 21:36:36 +0300

    added unit test for add animal button in header component

--- client-side/src/app/header/header.component.spec.ts
+++ client-side/src/app/header/header.component.spec.ts
@@ -1,9 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let headerComponent: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let addAnimalButton: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +18,16 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     headerComponent = fixture.componentInstance;
+    const selectedButton = fixture.debugElement.query(By.css('.header__buttons__add-animal'));
+    addAnimalButton = selectedButton.nativeElement;
     fixture.detectChanges();
   });
 
   it('should instantiate component', () => {
-    expect(headerComponent).not.toBeNull();
+    expect(headerComponent).toBeTruthy();
+  });
+
+  it('should have add animal button', () => {
+    expect(addAnimalButton.textContent).toBeDefined();
   });
 });
